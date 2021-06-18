db189f35808e14117c90f49f7e5aa7c95d1f0a33
Eliana Vargas Rojas <evargasrojas11@gmail.com>
2021-06-18 00:57:25 -0400

    Primer commit

---
+++ src/test/java/bo/edu/ucb/ingsoft/ProyectoFronted/ProyectoFrontedApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.ProyectoFronted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFrontedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
